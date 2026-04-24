cbffeb9a05d463990b20d77cd9b39f25d3484a25
Jake-beadle <jake.beadle@britishsteel.co.uk>
2026-04-24 11:26:45 +0100

    Moved javascript
    Moved the javascript to separate files to make the code cleaner/easier to read

--- login.php
+++ login.php
@@ -10,7 +10,8 @@ if (isset($_SESSION['user']) && isset($_SESSION['perms'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style.css">
-    <script src="./jquery-3.7.1.min.js"></script>
+    <script src="./scripts/jquery-3.7.1.min.js"></script>
+    <script src="./scripts/login.js"></script>
     <title>Login page</title>
 </head>
 <body>
@@ -28,23 +29,3 @@ if (isset($_SESSION['user']) && isset($_SESSION['perms'])){
     <p id="result">
 </body>
 </html>
-
-<script>
-    $(document).ready(function(){
-        $("#login").submit(function(event){
-            event.preventDefault();
-            var data = $(this).serializeArray();
-                $.post("./methods/accountlogin.php", data, function(response) {
-                    // Once the data for logging in has been submitted, accountlogin.php will return a response depending on what happened (saved here)
-                    var res = JSON.parse(response);
-                    // If the details entered were valid, it will have set the session data, so the user should be redirected to the main page
-                    if (res.status == "ok") {
-                        window.location.href = "/main.php"
-                    // If the details weren't valid, it returns a message explaining what went wrong to the user
-                    } else {
-                        $("#result").html(res.msg)
-                    }
-            })
-        })
-    })
-</script>
